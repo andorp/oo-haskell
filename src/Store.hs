5abbf305756db800dff78c73da10897d6bc56880
Andor Penzes <andor.penzes@gmail.com>
2016-08-03 13:00:47 +0200

    Intorduce Store class.

--- src/Store.hs
+++ src/Store.hs
@@ -1,17 +1,34 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Store
     ( newStore
     ) where
 
 import Data.IORef
 
+
+-- Object oriented programming is based on ad-hoc polimorphism
+-- A class in OO languages has two interpretation
+--  * a class serves as a type
+--  * a class serves as a collection of methods (things...)
+-- Haskell type classes are the way how we express ad-hoc polymorphism.
+
+-- We need a class...
+class Store s where
+    -- constructor
+    store :: a -> IO (s a)
+    -- methods
+    save  :: s a -> a -> IO ()
+    load  :: s a -> IO a
+
+
 -- It looks like an OO object...
-data Store a = Store {
-      save :: a -> IO ()
-    , load :: IO a
+data StoreObj a = StoreObj {
+      save_ :: a -> IO ()
+    , load_ :: IO a
     }
 
 -- It looks like a factory method...
-newStore :: a -> IO (Store a)
+newStore :: a -> IO (StoreObj a)
 newStore x = do
     storeRef <- newIORef x
 
@@ -19,12 +36,18 @@ newStore x = do
 
     let loadImp = readIORef storeRef
 
-    return $ Store saveImp loadImp
+    return $ StoreObj saveImp loadImp
+
+
+instance Store StoreObj where
+    store = newStore
+    save  = save_
+    load  = load_
 
 
 main :: IO ()
 main = do
-    store <- newStore 0
-    save store 3
-    x <- load store
+    s :: StoreObj Int <- store 0
+    save s 3
+    x <- load s
     print x
