9336d75ba93e42791cc541b2d441396290356085
Andor Penzes <andor.penzes@gmail.com>
2016-08-03 12:39:54 +0200

    Simple store exposes store implementation.

--- src/Store.hs
+++ src/Store.hs
@@ -1,5 +1,25 @@
-module Store where
+module Store
+    ( newStore
+    , save
+    , load
+    ) where
+
+import Data.IORef
+import System.IO.Unsafe (unsafePerformIO)
+
+newStore :: a -> IO (IORef a)
+newStore = newIORef
+
+save :: IORef a -> a -> IO ()
+save = writeIORef
+
+load :: IORef a -> IO a
+load = readIORef
 
 main :: IO ()
 main = do
-    putStrLn "Start"
+    store <- newStore 0
+    save store 3
+    x <- load store
+    print x
+
